6b70c01d5e133bbf4c1f68b47a5d68a4c02d552c
afifahrayyana <afifahrrizqi@gmail.com>
2026-03-29 00:30:30 +0700

    upgrade background lagi

--- portofolio-Afifah/src/components/LoadingScreen.tsx
+++ portofolio-Afifah/src/components/LoadingScreen.tsx
@@ -20,7 +20,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ onFinish }) => {
         alignItems: "center",
         height: "100vh",
         flexDirection: "column",
-        backgroundColor: " hsl(185 65% 50%)",
+        backgroundColor: " hsl(195 70% 55%)",
       }}
     >
       <Lottie
