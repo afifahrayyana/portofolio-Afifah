33976af65ecba09c6e2b9de7b062875c2cb58ed0
afifahrayyana <afifahrrizqi@gmail.com>
2026-03-29 00:27:31 +0700

    background

--- portofolio-Afifah/src/components/LoadingScreen.tsx
+++ portofolio-Afifah/src/components/LoadingScreen.tsx
@@ -20,7 +20,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ onFinish }) => {
         alignItems: "center",
         height: "100vh",
         flexDirection: "column",
-        backgroundColor: "hsl(200 60% 42%)",
+        backgroundColor: " hsl(185 65% 50%)",
       }}
     >
       <Lottie
